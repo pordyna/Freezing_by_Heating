bf656c8c78eb614845567e2d94348b4f57fecf13
Pawel Ordyna <pawel.ordyna@mailbox.tu-dresden.de>
2019-05-19 18:55:03 +0200

    changes(mistakes), justified docstring, + straight corridor callables

--- freezing_by_heating.py
+++ freezing_by_heating.py
@@ -14,33 +14,50 @@ def central_difference_derivative(function, x_0, h):
 def calc_gradient(function, vec_val, h, *args):
     gradient = np.array([0, 0])
     gradient[0] = central_difference_derivative(
-        lambda x_0: function(np.array[x_0, vec_val[1]], *args), vec_val[1], h)
+        lambda x_0: function(np.array[x_0, vec_val[1]], *args), vec_val[0], h)
     gradient[1] = central_difference_derivative(
-        lambda x_0: function(np.array[vec_val[0], x_0], *args), vec_val[0], h)
+        lambda x_0: function(np.array[vec_val[0], x_0], *args), vec_val[1], h)
     return gradient
 
 
+# later use `functools.partial` to set parameters (width, length)
+def straight_corridor_walls(position, corridor_width):
+    """ """
+    return min(position[1], corridor_width - position[1])
+
+
+def straight_corridor_periodic(state, corridor_length):
+    """ """
+    np.mod(state, corridor_length, where=[True, False], out=state)
+    return state
+
+
+def straight_corridor_direction(_position):
+    return np.array([1, 0])
+
+
 class Simulation:
-    # TODO adjust docstring length to PEP8
     """
 
     Some attributes:
-        initial_state(np.ndarray[dim=3]): Describes particles positions and
-          momenta. 1st axis 1st element position, 1st axis 2nd element
-          momentum, 2nd axis - particles, 3rd axis spatial components (x,y).
-          Example:
-                   x[1][n][0] is p_x of the n-th particle.
-                   x[0][n][1] is x_y of the n-th particle.
+        initial_state(np.ndarray[dim=3]): Describes particles positions
+          and momenta. 1st axis 1st element position, 1st axis 2nd
+          element momentum, 2nd axis - particles, 3rd axis spatial
+          components (x,y). Example:
+          * x[1][n][0] is p_x of the n-th particle.
+          * x[0][n][1] is x_y of the n-th particle.
           Particles with a positive desired velocity come first.
-          n_positive: Number of particles with the desired velocity > 0.
+        n_positive: Number of particles with the desired velocity > 0.
         walls(Callable[[np.ndarray], float]): For a given particle
-          position should return the shortest distance to the nearest wall.
-        boundary_condition(Callable[[np.ndarray], np.ndarrayvalue]): Takes a
-          state (as defined for initial_state) and returns an adapted state
-          that satisfies the boundary conditions.
-        desired_direction(Callable[[np.ndarray], np.ndarray]): For a given
-          position returns a unit vector pointing in the direction of the
-          desired movement (for particles with a positive desired velocity).
+          position should return the shortest distance to the nearest
+          wall.
+        boundary_condition(Callable[[np.ndarray], np.ndarray]): Takes a
+          state (as defined for initial_state) and returns an adapted
+          state that satisfies the boundary conditions.
+        desired_direction(Callable[[np.ndarray], np.ndarray]): For a
+          given position returns a unit vector pointing in the direction
+          of the desired movement (for particles with a positive desired
+          velocity).
     """
     def __init__(self, initial_state, n_positive,
                  desired_speed, desired_direction, relaxation_time,
@@ -49,19 +66,19 @@ class Simulation:
 
         self.initial_state = initial_state
         self.desired_speed = desired_speed
+        self.desired_direction = desired_direction
         self.relaxation_time = relaxation_time
         self.noise_amplitude = noise_amplitude
         self.param_factor = param_factor
         self.param_exponent = param_exponent
         self.core_diameter = core_diameter
         self.particle_mass = particle_mass
+        self.gradient_step = gradient_step
+        self.walls = walls
 
         self.n_particles = self.initial_state.shape[1]
         self.n_positive = n_positive
         self.n_negative = self.n_particles - self.n_positive
-        self.walls = walls
-        self.desired_direction = desired_direction
-        self.gradient_step = gradient_step
 
         self.ode_left_hand_side = np.zeros_like(self.initial_state)
         self.times = None
@@ -82,13 +99,13 @@ class Simulation:
     def _ppi_before_gradient(self, position, second_position):
         particles_distance = np.linalg.norm(second_position - position)
         value = particles_distance - self.core_diameter / 2
-        value = value**self.param_exponent
+        value = value**(-1 * self.param_exponent)
         value *= self.param_factor
         return value
 
     def _pbi_before_gradient(self, position):
         value = self.walls(position) - self.core_diameter / 2
-        value = value**self.param_exponent
+        value = value**(-1 * self.param_exponent)
         value *= self.param_factor
         return value
 
@@ -99,8 +116,8 @@ class Simulation:
             # one could exclude here the i=j case (see paper) but the
             # contribution is 0 anyway.
             for second_position in positions:
-                summed += calc_gradient(self._ppi_before_gradient, position,
-                                        self.gradient_step, second_position)
+                summed += - calc_gradient(self._ppi_before_gradient, position,
+                                          self.gradient_step, second_position)
             output[ii] = summed
         return output
 
@@ -113,7 +130,7 @@ class Simulation:
                 step = distance
             else:
                 step = self.gradient_step
-            output[ii] = calc_gradient(self._pbi_before_gradient, position,
+            output[ii] = - calc_gradient(self._pbi_before_gradient, position,
                                        step)
         return output
 
@@ -153,6 +170,8 @@ class Simulation:
         if self.simulation_succeeded:
             self.times = output.t
             self.states = output.y
+        else:
+            print('no success')
         # One can also try the continuous solution.
 
     def animate(self):
